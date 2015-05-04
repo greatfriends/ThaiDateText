94503bdf6d33dd7a5ebe756aea60d46a800b74b1
Suthep Saengwirotjanapat <suthep@gfbd.co.th>
2015-05-04 23:46:25 +0700

    Initial commit

---
+++ GFDN.ThaiDateText/ThaiDateTextUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GFDN.ThaiDateText {
+  public static class ThaiDateTextUtil {
+
+    public static string ThaiDateText(this DateTime[] dates) {
+      return "";
+    }
+
+  }
+}
