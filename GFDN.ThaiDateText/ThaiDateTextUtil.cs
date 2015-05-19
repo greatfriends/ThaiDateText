0574654cf762f5f68e7980bcfcfbe34707d0d2cb
Suthep Saengwirotjanapat <suthep@gfbd.co.th>
2015-05-19 23:24:23 +0700

    get started with basic form

--- GFDN.ThaiDateText/ThaiDateTextUtil.cs
+++ GFDN.ThaiDateText/ThaiDateTextUtil.cs
@@ -6,15 +6,7 @@ using System.Threading.Tasks;
 
 namespace GreatFriends.ThaiDateText {
   public static class ThaiDateTextUtil {
-    
-    public static string ThaiDateText(this DateTime date) { 
-      return string.Empty;
-    }
-    
-    public static string ThaiDateText(this DateTime startDate, DateTime endDate) {
-      return string.Empty;
-    }
-
+     
     public static string ThaiDateText(this IEnumerable<DateTime> dates) {
       return "";
     }
